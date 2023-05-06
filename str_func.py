b48d2920df39c0bc212d69533c8731e66bcc1702
Усман <salikhov_magomed@internet.ru>
2023-05-07 00:01:54 +0300

    Добавлена докстрингов

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def upper_case(string):
+    """
+    функция, которая делает заглавными первые буквы каждого слова в строке
+    """
     return string.upper()
+
+def capitalize_words(string):
+    """
+    Преобразует каждое слово в строке,
+    поступившей на вход функции, так,
+    чтобы первая буква каждого слова была заглавной.
+    """
+    return ' '.join(word.capitalize() for word in string.split())
